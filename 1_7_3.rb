ae62dbe8f5c11fb70b6ecd2c9150f4930b9e2504
kaoritorikai23 <kaori.torikai@gmail.com>
2021-08-23 22:21:18 +0900

    Second commit

---
+++ 1_7_3.rb
@@ -0,0 +1,16 @@
+class User
+    attr_accessor :name
+end
+user1 = User.new
+user1.name = "大場寧子"
+user2 = User.new
+user2.name = "小芝美由紀"
+user3 = User.new
+user3.name = "小田井優"
+users = [user1, user2, user3]
+
+names = users.map do |user|
+    user.name
+end
+
+p names
